905222222f3bc59f8a78b0ffc2d95d8ecd5d44d3
JoaoLuizGL <joaoluiz.glira@gmail.com>
2024-08-16 17:30:22 -0300

    commit 1

---
+++ PiramideAsteriscos/Program.cs
@@ -0,0 +1,40 @@
+﻿public class PiramideAsteriscos
+{
+    public void piramideNormal(int max)
+    {
+        string piramide = "";
+        for (int i = 0; i < max; i++)
+        {
+            piramide += "*";
+            Console.WriteLine(piramide);
+        }
+        Console.ReadKey();
+    }
+
+    public void piramideReversa(int max)
+    {
+        string piramide = "";
+        for (int i = 0; i < max; i++)
+        {
+            piramide += "*";
+        }
+
+        for (int i = max; i >= 1; i--)
+        {
+            Console.WriteLine(piramide.Remove(i));
+            
+        }
+        Console.ReadKey();
+    }
+
+    public static void Main()
+    {
+        int max;
+        Console.Write("Informe o tamanho máximo pirâmide: ");
+        max = int.Parse(Console.ReadLine());
+        PiramideAsteriscos piramide = new PiramideAsteriscos();
+        piramide.piramideNormal(max);
+        piramide.piramideReversa(max);
+
+    }
+}
